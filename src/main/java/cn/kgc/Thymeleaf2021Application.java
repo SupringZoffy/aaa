5584746adb4e543a85671b2e7ab45195d503aa5f
unknown <2802036415@qq.com>
2021-01-19 17:03:55 +0800

    Initial commit

---
+++ src/main/java/cn/kgc/Thymeleaf2021Application.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleaf2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Thymeleaf2021Application.class, args);
+    }
+
+}
